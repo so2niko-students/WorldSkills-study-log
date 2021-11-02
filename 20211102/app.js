bfc879e51013b4d341698f0c37ededb7f8a44830
Nick Sotula <so2niko@gmail.com>
2021-11-02 17:34:00 +0200

    docs: operators, README.md

---
+++ 20211102/app.js
@@ -0,0 +1,55 @@
+// var
+// let
+// const //константы
+
+let nameA = 'Артем';
+// console.log('Hello, ' + nameA);
+
+// nameA = 'София';
+
+/*
+Типы данных:
+?Числа - Number 
+1, 34, -100, 0.3, 4534534.66, new Number()
+
+BigInt
+1n, -100n, 47685934876948766n, new BigInt()
+
+?Строки - String ('', "", ``)
+'254452345', 'Hello', '', "--#^&%$fgdgs dfsdf gsdf g--", new String()
+
+?Логический - Boolean
+true, false, new Boolean()
+
+?Объект - Object
+{ name : 'Andrii' }, new Object()
+
+Символ - Symbol
+new Symbol()
+
+Null
+
+Undefined
+*/
+
+//Операторы
+nameA = 'София';
+//операнд(nameA) оператор(=) операнд('София') -- операция присваивания
+
+let count = 0;
+//    3       13    14  13   14
+count = count + 100 * 9 - 12 / 3;
+//                 900
+//                          4
+//            900
+//                     896
+// count <- 906
+//операнд(count) оператор(=) (операнд(count) оператор(+) операнд(100))
+
+//? Количество операндов
+// унарные - один операнд
+
+// бинарные - два операнда
+
+// тернарные - три операнда
+// a > 100 ? console.log('aaaa') : console.log('bbb')
